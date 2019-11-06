83247b011ffc89f34f35d4b43f1717636a299693
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-11-06 22:50:40 +0200

    not bad snapping

--- TransformingCollectionViewCells/ViewController.swift
+++ TransformingCollectionViewCells/ViewController.swift
@@ -17,12 +17,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return _observers.compactMap({ $0.observer })
     }
     private func addScrollViewObserver(_ observer: ScrollViewObserver) {
-        _observers.append(ScrollViewObserverBox(observer: observer))
+        _observers.appendUnique(ScrollViewObserverBox(observer: observer))
     }
     
     @IBOutlet private weak var collectionView: UICollectionView!
     
     // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.decelerationRate = .fast
+    }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         scrollViewDidScroll(collectionView)
@@ -36,18 +40,40 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
         addScrollViewObserver(cell)
+        cell.isHidden = indexPath.row == 0 || indexPath.row == data.count - 1
+        cell.label.text = "\(data[indexPath.row])"
         return cell
     }
     
     // MARK: UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         observers.forEach {
             $0.scrollViewDidScroll(scrollView)
         }
     }
     
+    private var index: CGFloat = 0
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let cellSize = self.cellSize(scrollView: scrollView)
+        var targetIndex = (targetContentOffset.pointee.x + cellSize.width / 2) / cellSize.width
+        targetIndex = velocity.x > 0 ? ceil(targetIndex) : floor(targetIndex)
+        targetIndex = targetIndex.clamped(min: 0, max: CGFloat(data.count - 1))
+        targetContentOffset.pointee.x = targetIndex * cellSize.width
+        
+        index = targetIndex
+    }
+    
     // MARK: UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSize(scrollView: collectionView)
+    }
+    
+    private func cellSize(scrollView: UIScrollView) -> CGSize {
         return CGSize(width: collectionView.bounds.width / 3, height: collectionView.bounds.height)
     }
 }
@@ -62,6 +88,7 @@ struct ScrollViewObserverBox {
 
 class Cell: UICollectionViewCell, ScrollViewObserver {
     
+    @IBOutlet weak var label: UILabel!
     @IBOutlet private weak var view: UIView!
     
     override func awakeFromNib() {
@@ -91,5 +118,12 @@ class Cell: UICollectionViewCell, ScrollViewObserver {
 extension CGFloat {
     func clamped(min: CGFloat, max: CGFloat) -> CGFloat {
         return Swift.min(max, Swift.max(min, self))
+    }
+}
+
+extension Array where Element == ScrollViewObserverBox {
+    mutating func appendUnique(_ element: Element) {
+        guard !contains(where: { $0.observer === element.observer }) else { return }
+        append(element)
     }
 }
